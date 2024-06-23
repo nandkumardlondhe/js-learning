9121e20370f1bea16ceaad752e24dadc025874f0
nandkumardlondhe <nandkumardlondhe@gmail.com>
2024-06-23 12:00:37 +0530

    23June2024- Data Conversion and Comparison

---
+++ beginning/DataConversion.js
@@ -0,0 +1,22 @@
+console.log("Testing remote repo");
+
+
+
+
+//Arthematic conversion in java script
+console.log(2+2);
+console.log(2-2);
+console.log(3%2);
+console.log(2*2);
+console.log(3**2);   //power
+
+
+let str1= "Nandkumar";
+let str2= "Londhe";
+
+console.log(str1+str2);
+
+console.log("1"+2+2);
+console.log("2"+3+3);
+console.log(3+"2");
+console.log(3+5+"9");
